90273e8d2207f17d9d15e4e81f50e9efd9e86f27
rohit <rohitparih@gmail.com>
2024-01-21 21:37:04 +0530

    Added

--- src/components/DummyFile.jsx
+++ src/components/DummyFile.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
 const DummyFile = () => {
-  return (
-    <div>DummyFile</div>
-  )
+    return (
+        <>
+            <div>
+                git init
+                git add .
+                git commit -m "your message"
+                git branch -M main
+                git remote add origin https://github.com/beingrohit-exe/dummy-repository.git
+                git push -u origin main
+            </div>
+            <div>
+                git add .
+                git commit -m "your message"
+                git push -u origin branch-name
+            </div>
+            <div>
+                git add . - Means adding all the files
+                git commit -m "message" - Adding message to particular commit
+                git fetch - Get the changes from origin
+                git pull -
+            </div>
+        </>
+    )
 }
 
 export default DummyFile
